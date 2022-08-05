3ccc8d3ed3c3293e7467637679c9f12a0fd3f359
hoon2hooni <sanghoon.engineer@gmail.com>
2022-08-05 09:57:20 +0900

    fix: data-cy제공 할 수 있게 변경

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-import nfts from "./nft.json" assert { type: "json" };
+const nfts = [
+  { name: "Dog 15", price: "0.101", color: "#758ead" },
+  { name: "Dog 13", price: "0.318", color: "#56fe0e" },
+  { name: "Cow 11", price: "0.369", color: "#4f86e2" },
+  { name: "Lion 18", price: "0.691", color: "#678844" },
+  { name: "Lion 13", price: "0.435", color: "#1b6e40" },
+  { name: "Lion 15", price: "0.115", color: "#86d2df" },
+  { name: "Lion 16", price: "0.773", color: "#d9b9c5" },
+  { name: "Lion 21", price: "0.816", color: "#c50542" },
+  { name: "Lion 19", price: "0.285", color: "#ab4df4" },
+  { name: "Dog 44", price: "0.423", color: "#9c6521" },
+  { name: "Dog 53", price: "0.900", color: "#ab00fa" },
+  { name: "Cat 77", price: "0.566", color: "#fb335a" },
+  { name: "Cat 55", price: "0.557", color: "#3af26c" },
+  { name: "Cat 13", price: "0.256", color: "#fd4b0b" },
+  { name: "Cat 12", price: "0.285", color: "#8b248e" },
+  { name: "Fish 13", price: "0.528", color: "#c40682" },
+  { name: "Fish 55", price: "0.392", color: "#4be659" },
+  { name: "Fish 99", price: "0.635", color: "#f7c83b" },
+];
 
 //global elements
 const inputEl = document.querySelector("#input");
-let mainEl = document.querySelector("main");
 const sortEl = document.querySelector(".sort-buttons");
-let searchBarEl = document.querySelector(".search-bar");
-
-mainEl.basicChildrenCounts = mainEl.children.length;
-searchBarEl.basicChildrenCounts = searchBarEl.children.length;
+const nftsEl = document.querySelector(".nfts");
+const autocompletesUl = document.querySelector(".search-bar__autocompletes");
 
 const nameEl = sortEl.children[0];
 const priceEl = sortEl.children[1];
@@ -18,28 +34,29 @@ let inputValue = "";
 let searchedNfts = [];
 let autocompletes = [];
 
+//type event
 inputEl.addEventListener("keyup", typeInput);
+
+//search events
+iconEl.addEventListener("click", handleClickSearch);
+autocompletesUl.addEventListener("click", (e) => {
+  inputEl.value = e.target.textContent;
+  handleClickSearch();
+});
+
+//sort events
 nameEl.addEventListener("click", handleClickSort(sorByName, "name"));
 priceEl.addEventListener("click", handleClickSort(sortByPrice, "price"));
-iconEl.addEventListener("click", handleClickSearch);
 
 function typeInput() {
   inputValue = inputEl.value;
   searchedNfts = getSearchedNfts(nfts, inputValue);
   if (inputEl.value) {
     autocompletes = searchedNfts.map(({ name }) => name);
-    replaceElement(searchBarEl, autocompletes, generateAutocompletesEl);
+    replaceElement(autocompletes, autocompletesUl, getAutocomplete);
   } else {
-    resetElement(searchBarEl);
+    resetElement(autocompletes);
   }
-}
-
-function handleClickSort(callback, sortBy) {
-  return () => {
-    searchedNfts.sort(callback);
-    replaceElement(mainEl, searchedNfts, generateNftsEl);
-    toggleClickedSortButtons(sortBy);
-  };
 }
 
 function handleClickSearch() {
@@ -48,86 +65,31 @@ function handleClickSearch() {
     return;
   }
   searchedNfts = getSearchedNfts(nfts, inputValue);
-  replaceElement(mainEl, searchedNfts, generateNftsEl);
+  replaceElement(searchedNfts, nftsEl, getNft);
   toggleShownSortButtons(searchedNfts);
-  resetElement(searchBarEl);
+  resetElement(autocompletesUl);
   resetClickedSortButtons();
 }
 
-function sorByName(n, m) {
-  return n.name > m.name ? 1 : -1;
+function getSearchedNfts(nfts, inputValue) {
+  return nfts.filter(({ name }) =>
+    name.toLowerCase().includes(inputValue.toLowerCase())
+  );
 }
 
-function sortByPrice(n, m) {
-  const a = Number(n.price);
-  const b = Number(m.price);
-  return a < b ? 1 : -1;
-}
-
-function replaceElement(element, data, callback) {
+function replaceElement(data, element, getElementCallback) {
   resetElement(element);
-  appendChildrenWithCallback(element, data, callback);
+  appendChildren(data, element, getElementCallback);
 }
 
 function resetElement(element) {
-  if (element.children.length !== element.basicChildrenCounts) {
-    element.removeChild(element.lastElementChild);
-  }
+  element.innerHTML = "";
 }
 
-function appendChildrenWithCallback(element, data, callback) {
-  if (data.length) {
-    const children = callback(data);
-    element.appendChild(children);
+function appendChildren(data, element, getElementCallback) {
+  for (const datum of data) {
+    element.innerHTML += getElementCallback(datum);
   }
-}
-
-function toggleShownSortButtons(filteredNfts) {
-  if (filteredNfts.length) {
-    sortEl.classList.remove("hidden");
-  } else {
-    sortEl.classList.add("hidden");
-  }
-}
-
-function toggleClickedSortButtons(buttonName) {
-  if (buttonName === "name") {
-    nameEl.classList.add("clicked");
-    priceEl.classList.remove("clicked");
-  } else {
-    nameEl.classList.remove("clicked");
-    priceEl.classList.add("clicked");
-  }
-}
-
-function resetClickedSortButtons() {
-  nameEl.classList.remove("clicked");
-  priceEl.classList.remove("clicked");
-}
-
-function generateNftsEl(nfts) {
-  const nftsEl = document.createElement("div");
-  nftsEl.classList.add("nfts");
-  nftsEl.setAttribute("data-cy", "nfts");
-  for (const nft of nfts) {
-    nftsEl.innerHTML += getNft(nft);
-  }
-  return nftsEl;
-}
-
-function generateAutocompletesEl(autocompletes) {
-  const autocompletesEl = document.createElement("ul");
-  autocompletesEl.classList.add("search-bar__autocompletes");
-  autocompletesEl.setAttribute("data-cy", "autocompletes");
-  for (const autocomplete of autocompletes) {
-    autocompletesEl.innerHTML += getAutocomplete(autocomplete);
-  }
-  autocompletesEl.addEventListener("click", (e) => {
-    inputEl.value = e.target.textContent;
-    handleClickSearch();
-  });
-
-  return autocompletesEl;
 }
 
 function getNft(nft) {
@@ -152,8 +114,43 @@ function getAutocomplete(autocomplete) {
   return `<li>${autocomplete}</li>`;
 }
 
-function getSearchedNfts(nfts, inputValue) {
-  return nfts.filter(({ name }) =>
-    name.toLowerCase().includes(inputValue.toLowerCase())
-  );
+function handleClickSort(callback, sortBy) {
+  return () => {
+    searchedNfts.sort(callback);
+    replaceElement(searchedNfts, nftsEl, getNft);
+    toggleClickedSortButtons(sortBy);
+  };
+}
+
+function sorByName(n, m) {
+  return n.name > m.name ? 1 : -1;
+}
+
+function sortByPrice(n, m) {
+  const a = Number(n.price);
+  const b = Number(m.price);
+  return a < b ? 1 : -1;
+}
+
+function toggleShownSortButtons(filteredNfts) {
+  if (filteredNfts.length) {
+    sortEl.classList.remove("hidden");
+  } else {
+    sortEl.classList.add("hidden");
+  }
+}
+
+function toggleClickedSortButtons(buttonName) {
+  if (buttonName === "name") {
+    nameEl.classList.add("clicked");
+    priceEl.classList.remove("clicked");
+  } else {
+    nameEl.classList.remove("clicked");
+    priceEl.classList.add("clicked");
+  }
+}
+
+function resetClickedSortButtons() {
+  nameEl.classList.remove("clicked");
+  priceEl.classList.remove("clicked");
 }
